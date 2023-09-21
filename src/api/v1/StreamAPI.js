f41aca5f89291fcc05f4d18890b2220b6410160b
cheonghakim <cheongha.kim@cyberone.kr>
2023-09-21 16:54:53 +0900

    fix: router, api class

---
+++ src/api/v1/StreamAPI.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import queryString from "query-string";
+
+export default class StreamAPI {
+  static v1 = "/stream/";
+  /**
+   * Stream API
+   */
+
+  /**
+   * 모델 데이터를 가져온다
+   * @param {string} prediction
+   * @param {BoxType} bbox
+   * @param {Date} date
+   * @param {string} time
+   * @param {string} model
+   * @param {number} line_num
+   *
+   * @typedef BoxType
+   * @property {number} left
+   * @property {number} top
+   * @property {number} width
+   * @property {number} height
+   * @returns {AxiosPromise<any>}
+   */
+  static getModelData({ prediction, bbox, date, time, model, line_num }) {
+    return axios.get(
+      `${this.v1}/infer/${queryString.stringify({
+        prediction,
+        bbox,
+        date,
+        time,
+        model,
+        line_num,
+      })}`
+    );
+  }
+}
